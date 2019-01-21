441bbc621b013bcf08b411ef5f30a8b44e91fa55
LDLRL <lomelek@gmail.com>
2019-01-21 02:14:03 -0300

    Initial got subtree commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Acá va solo la lógica
+ * 
+ */
+const filterSeasons = (dataEpisodes, condition) => {
+    const filteredSeasons = dataEpisodes.filter(element2 => {
+     return element2.seasonNum == condition
+    })
+    return filteredSeasons;
+  };
+
+  
+
+     
